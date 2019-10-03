09dadbe7b4b18a46fd864bab6666726635faf191
Patricia Vitoria Carrera <55083414+pvitoria@users.noreply.github.com>
2019-10-03 11:09:46 +0100

    Update config.py

--- SOURCE/config.py
+++ SOURCE/config.py
@@ -1,25 +1,23 @@
 import os
 
 # DIRECTORY INFORMATION
-DATASET = "imagenet"
-TEST_NAME ="Test1"
+DATASET = "_"
+TEST_NAME ="_"
 ROOT_DIR = os.path.abspath('../')
 DATA_DIR = os.path.join(ROOT_DIR, 'DATASET/'+DATASET+'/')
 OUT_DIR = os.path.join(ROOT_DIR, 'RESULT/'+DATASET+'/')
-MODEL_DIR = os.path.join(ROOT_DIR, 'MODEL/')
+MODEL_DIR = os.path.join(ROOT_DIR, 'MODEL/'+DATASET+'/')
 LOG_DIR = os.path.join(ROOT_DIR, 'LOGS/'+DATASET+'/')
 
-TRAIN_DIR = "train_full"
-TEST_DIR = "histo"
+TRAIN_DIR = "_"
+TEST_DIR = "_"
 
 # DATA INFORMATION
 IMAGE_SIZE = 224
 BATCH_SIZE = 10
 
-# RANDOM NUMBER GENERATOR INFORMATION
-SEED = 128
 
 # TRAINING INFORMATION
 USE_PRETRAINED = False
-PRETRAINED = "pretrained_file.ckpt"
+PRETRAINED = "_.h5"
 NUM_EPOCHS = 5
