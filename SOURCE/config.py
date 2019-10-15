f99d26e1cfccac770380c83830402afa381df78a
Patricia Vitoria Carrera <55083414+pvitoria@users.noreply.github.com>
2019-10-15 14:52:35 +0100

    Update config.py

--- SOURCE/config.py
+++ SOURCE/config.py
@@ -1,16 +1,16 @@
 import os
 
 # DIRECTORY INFORMATION
-DATASET = "_"
-TEST_NAME ="_"
+DATASET = "imagenet" # UPDATE
+TEST_NAME ="FirstTest"
 ROOT_DIR = os.path.abspath('../')
 DATA_DIR = os.path.join(ROOT_DIR, 'DATASET/'+DATASET+'/')
 OUT_DIR = os.path.join(ROOT_DIR, 'RESULT/'+DATASET+'/')
 MODEL_DIR = os.path.join(ROOT_DIR, 'MODEL/'+DATASET+'/')
 LOG_DIR = os.path.join(ROOT_DIR, 'LOGS/'+DATASET+'/')
 
-TRAIN_DIR = "_"
-TEST_DIR = "_"
+TRAIN_DIR = "train"  # UPDATE
+TEST_DIR = "test" # UPDATE
 
 # DATA INFORMATION
 IMAGE_SIZE = 224
@@ -19,5 +19,5 @@ BATCH_SIZE = 10
 
 # TRAINING INFORMATION
 USE_PRETRAINED = False
-PRETRAINED = "_.h5"
+PRETRAINED = "modelPretrained.h5" # UPDATE
 NUM_EPOCHS = 5
