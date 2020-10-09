eaabf5c75b2d60c673c579a7ed48f26bf37d964d
pvitoria <patricia.vitoria@upf.edu>
2020-10-09 18:05:17 +0200

    fix bug in dataloader

--- SOURCE/ChromaGAN.py
+++ SOURCE/ChromaGAN.py
@@ -9,7 +9,7 @@
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #GNU Affero General Public License for more details.
 #You should have received a copy of the GNU Affero General Public License
-#along with this program. If not, see <http://www.gnu.org/licenses/>. 
+#along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import tensorflow as tf
@@ -32,7 +32,7 @@ from keras.models import load_model, model_from_json, Model
 
 
 
-GRADIENT_PENALTY_WEIGHT = 10  
+GRADIENT_PENALTY_WEIGHT = 10
 
 
 
@@ -44,7 +44,7 @@ def deprocess(imgs):
 
 
 def reconstruct(batchX, predictedY, filelist):
-    
+
     result = np.concatenate((batchX, predictedY), axis=2)
     result = cv2.cvtColor(result, cv2.COLOR_Lab2BGR)
     save_results_path = os.path.join(config.OUT_DIR,config.TEST_NAME)
@@ -55,13 +55,13 @@ def reconstruct(batchX, predictedY, filelist):
     return result
 
 def reconstruct_no(batchX, predictedY):
-    
+
     result = np.concatenate((batchX, predictedY), axis=2)
     result = cv2.cvtColor(result, cv2.COLOR_Lab2BGR)
     return result
 
 def write_log(callback, names, logs, batch_no):
-    
+
     for name, value in zip(names, logs):
         summary = tf.Summary()
         summary_value = summary.value.add()
@@ -72,13 +72,13 @@ def write_log(callback, names, logs, batch_no):
 
 
 def wasserstein_loss(y_true, y_pred):
-    
+
     return tf.reduce_mean(y_pred)
 
 
 def gradient_penalty_loss(y_true, y_pred, averaged_samples,
                           gradient_penalty_weight):
-    
+
     gradients = K.gradients(y_pred, averaged_samples)[0]
     gradients_sqr = K.square(gradients)
     gradients_sqr_sum = K.sum(gradients_sqr,
@@ -87,9 +87,9 @@ def gradient_penalty_loss(y_true, y_pred, averaged_samples,
     gradient_penalty = gradient_penalty_weight * K.square(1 - gradient_l2_norm)
     return K.mean(gradient_penalty)
 
-    
+
 class RandomWeightedAverage(_Merge):
-    
+
     def _merge_function(self, inputs):
         weights = K.random_uniform((config.BATCH_SIZE, 1, 1, 1))
         return (weights * inputs[0]) + ((1 - weights) * inputs[1])
@@ -97,7 +97,7 @@ class RandomWeightedAverage(_Merge):
 class MODEL():
 
     def __init__(self):
-        
+
         self.img_shape_1 = (config.IMAGE_SIZE, config.IMAGE_SIZE, 1)
         self.img_shape_2 = (config.IMAGE_SIZE, config.IMAGE_SIZE, 2)
         self.img_shape_3 = (config.IMAGE_SIZE, config.IMAGE_SIZE, 3)
@@ -150,8 +150,8 @@ class MODEL():
         self.combined.compile(loss=['mse','kld', wasserstein_loss],
                             loss_weights=[1.0, 0.003, -0.1],
                             optimizer=optimizer) #1/300
-        
-        
+
+
         self.log_path= os.path.join(config.LOG_DIR,config.TEST_NAME)
         self.callback = TensorBoard(self.log_path)
         self.callback.set_model(self.combined)
@@ -164,7 +164,7 @@ class MODEL():
 
 
     def discriminator(self):
-        
+
         input_ab = Input(shape=self.img_shape_2, name='ab_input')
         input_l = Input(shape=self.img_shape_1, name='l_input')
         net = keras.layers.concatenate([input_l, input_ab])
@@ -183,7 +183,7 @@ class MODEL():
 
 
     def colorization_model(self):
-        
+
         input_img = Input(shape=self.img_shape_3)
 
 
@@ -245,32 +245,32 @@ class MODEL():
 
 
     def train(self, data,test_data, log,sample_interval=1):
-        
+
         # Create folder to save models if needed.
         save_models_path =os.path.join(config.MODEL_DIR,config.TEST_NAME)
         if not os.path.exists(save_models_path):
                 os.makedirs(save_models_path)
-                
-        # Load VGG network             
-        VGG_modelF = applications.vgg16.VGG16(weights='imagenet', include_top=True) 
-        
+
+        # Load VGG network
+        VGG_modelF = applications.vgg16.VGG16(weights='imagenet', include_top=True)
+
         # Real, Fake and Dummy for Discriminator
         positive_y = np.ones((config.BATCH_SIZE, 1), dtype=np.float32)
         negative_y = -positive_y
         dummy_y = np.zeros((config.BATCH_SIZE, 1), dtype=np.float32)
-        
+
         # total number of batches in one epoch
         total_batch = int(data.size/config.BATCH_SIZE)
 
         for epoch in range(config.NUM_EPOCHS):
                 for batch in range(total_batch):
                     # new batch
-                    trainL, trainAB, _ = data.generate_batch()
+                    trainL, trainAB, _, original, l_img_oritList  = data.generate_batch()
                     l_3=np.tile(trainL,[1,1,1,3])
-                    
+
                     # GT vgg
                     predictVGG =VGG_modelF.predict(l_3)
-                    
+
                     # train generator
                     g_loss =self.combined.train_on_batch([l_3, trainL],
                                                         [trainAB, predictVGG, positive_y])
@@ -280,44 +280,47 @@ class MODEL():
                     # update log files
                     write_log(self.callback, self.train_names, g_loss, (epoch*total_batch+batch+1))
                     write_log(self.callback, self.disc_names, d_loss, (epoch*total_batch+batch+1))
-                    
-                    if (batch+1)%1000 ==0: 
+
+                    if (batch)%1000 ==0:
                         print("[Epoch %d] [Batch %d/%d] [generator loss: %08f] [discriminator loss: %08f]" %  ( epoch, batch,total_batch, g_loss[0], d_loss[0]))
-          
                 # save models after each epoch
                 save_path = os.path.join(save_models_path, "my_model_combinedEpoch%d.h5" % epoch)
-                self.combined.save(save_path) 
+                self.combined.save(save_path)
                 save_path = os.path.join(save_models_path, "my_model_colorizationEpoch%d.h5" % epoch)
-                self.colorizationModel.save(save_path) 
+                self.colorizationModel.save(save_path)
                 save_path = os.path.join(save_models_path, "my_model_discriminatorEpoch%d.h5" % epoch)
-                self.discriminator.save(save_path) 
-                
+                self.discriminator.save(save_path)
+
                 # sample images after each epoch
-                self.sample_images(test_data)
+                self.sample_images(test_data,epoch)
 
 
-    def sample_images(self,test_data):
+    def sample_images(self,test_data,epoch):
         total_batch = int(test_data.size/config.BATCH_SIZE)
         for _ in range(total_batch):
                 # load test data
-                testL, _ ,  filelist  = test_data.generate_batch()
-                
+                testL, _ ,  filelist, original, labimg_oritList  = test_data.generate_batch()
+
                 # predict AB channels
                 predAB, _  = self.colorizationModel.predict(np.tile(testL,[1,1,1,3]))
-                
+
                 # print results
                 for i in range(config.BATCH_SIZE):
-                        predResult = reconstruct(deprocess(testL)[i], deprocess(predAB)[i], filelist[i][:-4] )
+                        originalResult =  original[i]
+                        height, width, channels = originalResult.shape
+                        predictedAB = cv2.resize(deprocess(predAB[i]), (width,height))
+                        labimg_ori =np.expand_dims(labimg_oritList[i],axis=2)
+                        predResult = reconstruct(deprocess(labimg_ori), predictedAB, "epoch"+str(epoch)+"_"+filelist[i][:-5] )
 
 
 
 if __name__ == '__main__':
-    
+
     # Create log folder if needed.
     log_path= os.path.join(config.LOG_DIR,config.TEST_NAME)
     if not os.path.exists(log_path):
         os.makedirs(log_path)
-        
+
     with open(os.path.join(log_path, str(datetime.datetime.now().strftime("%Y%m%d")) + "_" + str(config.BATCH_SIZE) + "_" + str(config.NUM_EPOCHS) + ".txt"), "w") as log:
         log.write(str(datetime.datetime.now()) + "\n")
 
@@ -326,10 +329,10 @@ if __name__ == '__main__':
         test_data = data.DATA(config.TEST_DIR)
         assert config.BATCH_SIZE<=train_data.size, "The batch size should be smaller or equal to the number of training images --> modify it in config.py"
         print("Train data loaded")
-        
+
         print("Initiliazing Model...")
         colorizationModel = MODEL()
         print("Model Initialized!")
-        
+
         print("Start training")
         colorizationModel.train(train_data,test_data, log)
