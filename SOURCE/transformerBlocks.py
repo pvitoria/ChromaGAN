1f54683d006f7b7bd7210b1dd977c444f1155e16
Azrenbeth <77782548+Azrenbeth@users.noreply.github.com>
2021-11-21 12:34:58 +0000

    removed dropouts from mlp

--- SOURCE/transformerBlocks.py
+++ SOURCE/transformerBlocks.py
@@ -83,7 +83,7 @@ class PatchEncoder(layers.Layer):
 def mlp(x, hidden_units, dropout_rate):
     for units in hidden_units:
         x = layers.Dense(units, activation=tf.nn.gelu)(x)
-        x = layers.Dropout(dropout_rate)(x)
+        # x = layers.Dropout(dropout_rate)(x)
     return x
 
 
